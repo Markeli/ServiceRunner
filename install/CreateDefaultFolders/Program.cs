8ade71199f035cd2b7caba9c9e1064a9ccade17f
Maxim Markelow <max.markelow@gmail.com>
2016-11-12 03:38:02 +0300

    Added installer

---
+++ install/CreateDefaultFolders/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateDefaultFolders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Process.Start("cmd", "mkdir %Public%/ServiceRunner/services/");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
